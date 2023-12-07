e816cf94836065cab0b2c3581627994c1e91db00
Guilherme Nunes Lobo <loboguilherme2003@gmail.com>
2023-12-07 14:43:18 -0300

    Ajuste na cifra

--- MiniChallenge.swiftpm/ViewModel/MusicChordViewModel.swift
+++ MiniChallenge.swiftpm/ViewModel/MusicChordViewModel.swift
@@ -12,21 +12,6 @@ class MusicChordViewModel: ObservableObject {
 
     init() {
         let lyrics = """
-            In the world of tunes and melodies,
-            Where guitar strings hum with ease.
-            There's a secret code, a musician's key,
-            It's called a chord, as easy as can be.
-
-            A letter or two, maybe even three,
-            Combined with rhythm, in sweet harmony.
-            Strum a C, a G, or an Am,
-            Each chord tells a story, a musical jam.
-
-            And as you play, with fingers agile,
-            Notes and lyrics, like a flowing mile.
-            A chord sheet guides, a player's friend,
-            From intro to outro, where stories blend.
-
             So here's to chords, a songwriter's delight,
             In the language of music, shining bright.
             Whether major or minor, they play their part,
